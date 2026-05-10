01d21e78ff77a929ebe3e6c2e6f394223b5ea0f4
tom-as <tom@as.com>
2026-05-10 21:03:16 +0200

    INIT: initial structure and files

---
+++ jobs/freestyle-demo.groovy
@@ -0,0 +1,7 @@
+job('demo-freestyle') {
+    description('Per Job DSL aus Git erzeugter Freestyle-Job')
+    label('')
+    steps {
+        shell('echo "Hallo aus dem generierten Job TEST TEST"')
+    }
+}
