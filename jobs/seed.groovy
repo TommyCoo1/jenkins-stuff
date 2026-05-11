fc283fd4d48e6a64c26217245f75e89eda88c26e
tom-as <tom@as.com>
2026-05-11 10:52:24 +0200

    chore:correction base setup build gradle

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -1,10 +1,10 @@
 job('seed-job') {
     description('Der Meta-Seed-Job: Generiert alle anderen Jobs aus dem Git-Repo und aktualisiert sich selbst.')
-    
+
     triggers {
         scm('H/5 * * * *') // all 5 min check repo for updates
     }
-    
+
     scm {
         git {
             remote {
@@ -13,17 +13,15 @@ job('seed-job') {
             branch('main')
         }
     }
-    
+
     steps {
+        shell('mkdir -p build/classes && groovyc -d build/classes src/main/groovy/util/JobBuilder.groovy')
+
         jobDsl {
             targets('jobs/**/*.groovy')
-            
             // classpath
-            additionalClasspath('src/main/groovy')
-            
-            // 
+            additionalClasspath('build/classes')
             sandbox(false)
-            
             // delete in repo -> delete job
             removedJobAction('DELETE')
             removedViewAction('DELETE')
