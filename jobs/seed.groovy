83348e36e00ebda5654f215be9763855154f2a73
tom-as <tom@as.com>
2026-05-11 09:32:16 +0200

    feat: add seed

---
+++ jobs/seed.groovy
@@ -0,0 +1,29 @@
+job('seed-job') {
+    description('Der Meta-Seed-Job: Generiert alle anderen Jobs aus dem Git-Repo und aktualisiert sich selbst.')
+    
+    triggers {
+        scm('H/5 * * * *') // all 5 min check repo for updates
+    }
+    
+    scm {
+        git {
+            remote {
+                url('https://github.com/TommyCoo1/jenkins-stuff.git')
+            }
+            branch('main')
+        }
+    }
+    
+    steps {
+        jobDsl {
+            targets('jobs/**/*.groovy')
+            
+            // classpath
+            additionalClasspath('src')
+            
+            // delete in repo -> delete job
+            removedJobAction('DELETE')
+            removedViewAction('DELETE')
+        }
+    }
+}
