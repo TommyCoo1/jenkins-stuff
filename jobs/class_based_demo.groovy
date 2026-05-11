8fa7363a715fad151568250b2c228370ff2fb30b
tom-as <tom@as.com>
2026-05-11 09:35:15 +0200

    removed TEST, uncomment

--- jobs/class_based_demo.groovy
+++ jobs/class_based_demo.groovy
@@ -1,35 +1,35 @@
-// import util.JobBuilder
+import util.JobBuilder
 
-// // def mainJob = new JobBuilder(
-// //     jobName: 'class-based-demo-job',
-// //     repoUrl: 'https://github.com/TommyCoo1/jenkins-stuff.git',
-// //     branch: 'main',
-// //     jenkinsfilePath: 'pipelines/demo.Jenkinsfile'
-// // )
-// // 'this' = Job DSL Skript
-// // mainJob.build(this)
+// def mainJob = new JobBuilder(
+//     jobName: 'class-based-demo-job',
+//     repoUrl: 'https://github.com/TommyCoo1/jenkins-stuff.git',
+//     branch: 'main',
+//     jenkinsfilePath: 'pipelines/demo.Jenkinsfile'
+// )
+// 'this' = Job DSL Skript
+// mainJob.build(this)
 
 
-// // multiple jobs
-// def microservices = ['auth-service', 'user-service', 'payment-service']
+// multiple jobs
+def microservices = ['auth-service', 'user-service', 'payment-service']
 
-// // microservices.each { serviceName ->
-// //     new JobBuilder(
-// //         jobName: "microservice-${serviceName}",
-// //         repoUrl: "https://github.com/TommyCoo1/${serviceName}.git",
-// //         jenkinsfilePath: 'Jenkinsfile'
-// //     ).build(this)
-// // }
-
-// // Folder for jobs
-// folder('microservices') {
-//     description('Beinhaltet alle Microservice-Build-Jobs')
-// }
-
-// // jobs to folder with prefix
 // microservices.each { serviceName ->
 //     new JobBuilder(
-//         jobName: "microservices/build-${serviceName}",
-//         repoUrl: "https://github.com/TommyCoo1/${serviceName}.git"
+//         jobName: "microservice-${serviceName}",
+//         repoUrl: "https://github.com/TommyCoo1/${serviceName}.git",
+//         jenkinsfilePath: 'Jenkinsfile'
 //     ).build(this)
 // }
+
+// Folder for jobs
+folder('microservices') {
+    description('Beinhaltet alle Microservice-Build-Jobs')
+}
+
+// jobs to folder with prefix
+microservices.each { serviceName ->
+    new JobBuilder(
+        jobName: "microservices/build-${serviceName}",
+        repoUrl: "https://github.com/TommyCoo1/${serviceName}.git"
+    ).build(this)
+}
