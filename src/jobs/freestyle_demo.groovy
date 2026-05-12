e9a53de6d008aa55bdc4f42eecb4b82bf02bfb17
tom-as <tom@as.com>
2026-05-12 09:32:45 +0200

    just a test

--- src/jobs/freestyle_demo.groovy
+++ src/jobs/freestyle_demo.groovy
@@ -4,6 +4,6 @@ job('demo-freestyle') {
     description('Per Job DSL aus Git erzeugter Freestyle-Job')
     label('')
     steps {
-        shell('echo "Hallo aus dem generierten Job TEST TEST"')
+        shell('echo "freeeeeeeeeeeeeeeeeee"')
     }
 }
